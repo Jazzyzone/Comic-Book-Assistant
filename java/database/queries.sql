1434952ad24116a9f8f2078f07c78184fc90486e
Jampiere Santos <santosjp16@gmail.com>
2021-04-05 16:00:42 -0400

    updated database

--- java/database/queries.sql
+++ java/database/queries.sql
@@ -67,50 +67,74 @@ CREATE TABLE comics (
 	title varchar(50) NOT NULL,
 	issue_num int NOT NULL,
 	series varchar(50) NOT NULL,
-	desc varchar(50) NOT NULL,
-	img varchar(200) NOT NULL
-	//CONSTRAINT PK_transfer_types PRIMARY KEY (transfer_type_id)
+	description varchar(50) NOT NULL,
+	img varchar(200) NOT NULL,
+	
+	CONSTRAINT PK_comics PRIMARY KEY (comic_id)
 );
 
-CREATE TABLE transfer_statuses (
-	transfer_status_id int DEFAULT nextval('seq_transfer_status_id'::regclass) NOT NULL,
-	transfer_status_desc varchar(10) NOT NULL,
-	CONSTRAINT PK_transfer_statuses PRIMARY KEY (transfer_status_id)
-);
+CREATE TABLE collections (
 
-CREATE TABLE users (
-	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
-	username varchar(50) NOT NULL,
-	password_hash varchar(200) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id),
-	CONSTRAINT UQ_username UNIQUE (username)
-);
-
-CREATE TABLE accounts (
-	account_id int DEFAULT nextval('seq_account_id'::regclass) NOT NULL,
+	collection_id int DEFAULT nextval('seq_collection_id'::regclass) NOT NULL,
 	user_id int NOT NULL,
-	balance decimal(13, 2) NOT NULL,
-	CONSTRAINT PK_accounts PRIMARY KEY (account_id),
-	CONSTRAINT FK_accounts_user FOREIGN KEY (user_id) REFERENCES users (user_id)
+	name varchar(50) NOT NULL,
+	hidden boolean DEFAULT false, --would we need this in the table or in the vue side?
+	
+	CONSTRAINT PK_collections PRIMARY KEY (collection_id)
 );
 
-CREATE TABLE transfers (
-	transfer_id int DEFAULT nextval('seq_transfer_id'::regclass) NOT NULL,
-	transfer_type_id int NOT NULL,
-	transfer_status_id int NOT NULL,
-	account_from int NOT NULL,
-	account_to int NOT NULL,
-	amount decimal(13, 2) NOT NULL,
-	CONSTRAINT PK_transfers PRIMARY KEY (transfer_id),
-	CONSTRAINT FK_transfers_accounts_from FOREIGN KEY (account_from) REFERENCES accounts (account_id),
-	CONSTRAINT FK_transfers_accounts_to FOREIGN KEY (account_to) REFERENCES accounts (account_id),
-	CONSTRAINT FK_transfers_transfer_statuses FOREIGN KEY (transfer_status_id) REFERENCES transfer_statuses (transfer_status_id),
-	CONSTRAINT FK_transfers_transfer_types FOREIGN KEY (transfer_type_id) REFERENCES transfer_types (transfer_type_id),
-	CONSTRAINT CK_transfers_not_same_account CHECK  ((account_from<>account_to)),
-	CONSTRAINT CK_transfers_amount_gt_0 CHECK ((amount>0))
+CREATE TABLE characters (
+
+	character_id int DEFAULT nextval('seq_character_id'::regclass) NOT NULL,
+	name varchar(50) NOT NULL,
+	
+	CONSTRAINT PK_characters PRIMARY KEY (character_id)
+	--CONSTRAINT UQ_username UNIQUE (username)
 );
 
+CREATE TABLE creator (
+
+	creator_id int DEFAULT nextval('seq_creator_id'::regclass) NOT NULL,
+	first_name varchar(50)NOT NULL,
+	last_name varchar(50) NOT NULL,
+	full_name varchar(50) NOT NULL,
+	
+        CONSTRAINT PK_creator PRIMARY KEY (creator_id)
+	--CONSTRAINT FK_accounts_user FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+CREATE TABLE series (
+
+	series_id int DEFAULT nextval('seq_series_id'::regclass) NOT NULL,
+	title varchar(50) NOT NULL,
+	description varchar(100) NOT NULL,
+	start_year int NOT NULL,
+	end_year int NOT NULL,
+	rating varchar(50) NOT NULL,
+	
+	CONSTRAINT PK_series PRIMARY KEY (series_id)
+	
+);
+
+CREATE TABLE user_info (
+
+	user_info_id int NOT NULL,
+	is_premium boolean DEFAULT false,
+	first_name varchar(50)NOT NULL,
+	last_name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	
+	CONSTRAINT PK_user_info PRIMARY KEY (user_info_id)
+);
+	
+CREATE TABLE publisher (
+
+	publisher_id int DEFAULT nextval('seq_publisher_id'::regclass) NOT NULL,
+	name varchar(50) NOT NULL,
+	
+	CONSTRAINT PK_publisher PRIMARY KEY (publisher_id)
+);
 ROLLBACK;
 
-//COMMIT TRANSACTION;
+--COMMIT TRANSACTION;
 
