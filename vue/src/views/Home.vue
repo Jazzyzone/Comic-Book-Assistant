77395c03d6c5ff86dbe35403284f3164d2dd70a9
Dennis Paskel <dpaskel@gmail.com>
2021-04-07 14:04:13 -0400

    added cover art to front page expanded nav links

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="home">
-    <!-- <h1>Welcome {{this.$store.state.user.username}}</h1> -->
-    <router-link :to="{ name: 'UserHome', params: {username: this.$store.state.user.username} }">View My Page</router-link>
-    <!-- <p>Add Comic Book Collection</p>
-    <p>Add Comic Book</p>
-    <p>My Friends</p>
-    <p>See Collections of Others</p> -->
+
+    <img src="../images/marvel_comics_cast.png" alt="" srcset="">
+
   </div>
 </template>
 
