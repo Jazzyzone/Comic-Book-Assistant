b9e8e5a9d45451e9f86bfedcee44459f67ab403e
Dennis Paskel <dpaskel@gmail.com>
2021-04-13 23:12:27 -0400

    merge issure again lol

--- vue/src/components/UserCollections.vue
+++ vue/src/components/UserCollections.vue
@@ -1,10 +1,9 @@
 //Display all collections a user has
 //if they select on the icon it will take
 //them to ComicsList
-
 <template>
 <v-container>
-    <v-row  justify="center" class="collectionDropdown">
+    <v-row  justify="center">
   <v-col class="shrink">
       <v-btn
         class="ma-2"
@@ -14,12 +13,11 @@
       >
         Add a Collection
       </v-btn>
-
       <v-expand-transition>
         <v-card
           v-show="expand"
           lighten3
-          class="mx-auto white mb-2"
+          class="mx-auto white mb-2 semi"
           width = "300"
           elevation=10
         >
@@ -28,22 +26,21 @@
       </v-expand-transition>
     </v-col>
    </v-row>
-  <v-expansion-panels accordion flat v-for="collection in collections"
-            v-bind:key="collection.collection_ID" class="collectionDropdown">
+  <v-expansion-panels accordion flat  dark>
             <!--added div below and moved v-for and v-bind from v-expansion panel to v-expansion panels
             in order to add a div that would only display elements if the user is signed in or the 
             collection is not private -->
-    <div v-if="isCurrentUser || !collection.private">
-    <v-expansion-panel
-      
+    <v-expansion-panel  
+      v-for="collection in filtered"
+            v-bind:key="collection.collection_ID"
+            class="mb-5"
     >
-      
-      <v-expansion-panel-header>
+      <v-expansion-panel-header  dark
+            color="primary" >
         {{collection.name}}
       </v-expansion-panel-header>
       <v-expansion-panel-content class="bg">
         <v-flex v-if="isCurrentUser" class="d-flex flex-row align-center">
- 
           <v-expansion-panels class="ma-1">
             <v-expansion-panel>
               <v-expansion-panel-header>
@@ -67,7 +64,6 @@
                           v-else
                           key="1"
                         >
-                         
                         </span>
                       </v-fade-transition>
                     </v-col>
@@ -78,12 +74,10 @@
                 <v-row>
                 <v-col  cols="8">
                 <v-text-field label="title" v-model="searchForT"
-
                 ></v-text-field>
                 </v-col>
                   <v-col  cols="4">
                     <v-text-field label="issue number" v-model="searchForI"
-
                 ></v-text-field>
                     </v-col>
                     </v-row>
@@ -91,7 +85,6 @@
               </v-expansion-panel-content>
             </v-expansion-panel>
              </v-expansion-panels>
-
           <div class="text-center ma-1">
             <v-dialog v-model="dialog" width="500">
               <template v-slot:activator="{ on, attrs }">
@@ -104,12 +97,10 @@
                   Edit
                 </v-btn>
               </template>
-
               <v-card>
                 <v-card-title class="headline grey lighten-2">
                   Edit Collection
                 </v-card-title>
-
                 <v-card-text>
                   <v-text-field v-model="rename" label="name"></v-text-field>                          
                 </v-card-text>
@@ -122,27 +113,18 @@
               </v-card>
             </v-dialog>
           </div>
-
             <v-btn color="red lighten-1" dark class="ma-1" @click="deleteCollection(collection.collectionID)">Delete</v-btn>
         </v-flex>
         <comics-list v-bind:collectionID="collection.collectionID"/>
       </v-expansion-panel-content>
     </v-expansion-panel>
-    </div>
   </v-expansion-panels>
-
 </v-container>
-
-    
 </template>
-
 <script>
-
 import ComicServices from '../services/ComicServices';
 import AddACollection from './AddACollection.vue';
 import ComicsList from './ComicsList.vue';
-
-
 export default {
   components: { AddACollection, ComicsList },
    data() {
@@ -154,7 +136,6 @@ export default {
      searchForT:"",
      rename:"",
      dialog : false,
-     nameChangeErrorMsg: "Your collection name must be at least 1 character"
        }
    },
     created() {
@@ -170,6 +151,14 @@ export default {
         isCurrentUser() {
             return this.$route.params.username === this.$store.state.user.username;
         },
+        filtered(){
+          return this.$store.state.userCollections.filter(item=>{
+            if(this.isCurrentUser||item.private==false){
+                return true;
+            }
+            return false;
+          })
+        }
     },
     methods: {
         deleteCollection(collectionId) {
@@ -190,23 +179,20 @@ export default {
             this.$router.push({ name: "ComicSearch", params: {collectionID: this.collectionid, title: this.searchForT, issue: this.searchForI}});
         },
         updateCollection(collection) {
-            collection.name = this.rename.trim();
-          
+            collection.name = this.rename
             ComicServices.updateCollection(collection).then(response => {
                 if(response.status == 200) {
                     this.changeId = '';
-                    this.nameChange = true;
+                    this.changeName = false;
                     ComicServices.getAllCollections(this.$route.params.username).then(response => {
              this.$store.commit("SET_COLLECTIONS", response.data)
          });
                 }
             });
-            
         },
     }
 }
 </script>
-
 <style scoped>
 .container {
   position: relative;
@@ -214,15 +200,12 @@ export default {
 }
 .transparent {
   opacity: 0.1;
-
 }
 .addCollection {
   width: 100px;
   white-space: nowrap;
 }
-.collectionDropdown div{
+.collectionDropdown {
   margin-bottom: 10px;
-  border: 0px solid black;
-  border-radius: 5px;
 }
 </style>
