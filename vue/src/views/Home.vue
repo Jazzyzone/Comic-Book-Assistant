d474229acc5a88c0c0bf4383143b822984c18338
Jasmine Dawkins <jasminedawkins2009@gmail.com>
2021-04-14 16:30:56 -0400

    saving work on homepage

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,13 +1,71 @@
 <template>
   <v-container  ma-0 pa-0 fill-height>
-    <img v-img src="@/images/marvel_comics_cast.png" class="homepage" />
 
-    <div>
-    <h1>Comic Elevator</h1>
-    <h3>
-      Make your own custom collections and add your favorite marvel comics to
+    <v-carousel v-model="model" 
+    cycle
+    class="container">
+    <v-carousel-item>
+      <v-sheet
+        color="indigo"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            <v-img 
+    src="@/images/marvel_comics_cast.png" class="homepage"></v-img>
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+      <v-carousel-item>
+      <v-sheet
+        color="deep-purple"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            <v-img 
+    src="@/images/captain_america_running.png" class="captain"></v-img>
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+     <v-carousel-item>
+      <v-sheet
+        color="purple"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            <v-img 
+    src="@/images/ironman_flying.png" class="ironman"></v-img>
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+
+    <div class="text">
+    <h2>Comic Elevator</h2>
+    <h4>
+      Make your own comic book collections and add your favorite marvel comics to
       them. Manage your comic book collections and see how your stats compare to other users.
-    </h3>
+    </h4>
     </div>
 
   </v-container>
@@ -16,7 +74,6 @@
 
 <script>
 export default {
-  components: {},
   data() {
     return {
       model: 0,
@@ -26,23 +83,33 @@ export default {
 };
 </script>
 <style>
-h1 {
+
+h2 {
   text-align: center;
   color: whitesmoke;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
 
-h3 {
+h4 {
   text-align: center;
   color: whitesmoke;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   overflow: visible;
 }
 
-.homepage {
-  width:auto;
-   height:auto;
+
+.ironman {
    max-width:100%;
-   max-height:100%;
+    max-height:100%;
+    vertical-align: middle;
+    width: 450px;
+    height: 450px;
+    line-height: 115px;
+    text-align: center;
 }
+
+.hide-scrollbar::-webkit-scrollbar {
+    display: none;
+}
+
 </style>
